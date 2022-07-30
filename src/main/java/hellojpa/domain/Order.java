1f8f80b654886f39d63c7a528b5e91e1ce4d54b7
kang <rilac1@naver.com>
2022-07-31 01:44:55 +0900

    :sparkles: 지연 로딩과 영속성 전이 적용

--- src/main/java/hellojpa/domain/Order.java
+++ src/main/java/hellojpa/domain/Order.java
@@ -4,10 +4,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -29,14 +31,14 @@ public class Order extends BaseEntity {
 	@Column(name = "ORDER_ID")
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "MEMBER_ID")
 	private Member member;
 
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 
-	@OneToOne
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "DELIVERY_ID")
 	private Delivery delivery;
 
